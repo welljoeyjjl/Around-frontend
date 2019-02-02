44cfa2c6284b106745d63254724091966df410cf
welljoeyjjl <welljoeyjjl@hotmail.com>
2019-02-02 18:50:01 -0500

    Register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {TopBar} from './TopBar';
-
+import {Main} from './Main.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <TopBar/>
+          <Main/>
       </div>
     );
   }
---
+++ src/Register.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import {
+    Form, Input, Button} from 'antd';
+
+class RegistrationForm extends React.Component {
+    state = {
+        confirmDirty: false,
+        autoCompleteResult: [],
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+
+
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Item
+                    {...formItemLayout}
+                    label="Username"
+                >
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
+                    })(
+                        <Input />
+                    )}
+                </Form.Item>
+                <Form.Item
+                    {...formItemLayout}
+                    label="Password"
+                >
+                    {getFieldDecorator('password', {
+                        rules: [{
+                            required: true, message: 'Please input your password!',
+                        }, {
+                            validator: this.validateToNextPassword,
+                        }],
+                    })(
+                        <Input type="password" />
+                    )}
+                </Form.Item>
+                <Form.Item
+                    {...formItemLayout}
+                    label="Confirm Password"
+                >
+                    {getFieldDecorator('confirm', {
+                        rules: [{
+                            required: true, message: 'Please confirm your password!',
+                        }, {
+                            validator: this.compareToFirstPassword,
+                        }],
+                    })(
+                        <Input type="password" onBlur={this.handleConfirmBlur} />
+                    )}
+                </Form.Item>
+
+
+                <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">Register</Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+
+export const Register = Form.create({ name: 'register' })(RegistrationForm);
